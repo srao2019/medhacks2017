8be6dcbc0e06afd6f500937e86a809ffd8d8038e
Subhashini Arumugam <subhashinia97@gmail.com>
2017-09-09 19:30:09 -0400

    2pmmedhack

--- main.php
+++ main.php
@@ -19,7 +19,7 @@
 						<strong>Username: </strong><input type="text" name="username" /><br /><br />
 						<strong>Password: </strong><input type="password" name="password"/><br /><br />
 						<input type="submit" name="login" value = "Login" /><br/>
-                    </form>	
+				</form>	
 EOBODY;
 			}
             $bottom = "";
@@ -35,9 +35,9 @@ EOBODY;
                     }else{
                         $bottom = "";
                     }
-                }if($bottom === ""){
-                    if(!(isset($_SESSION['username'])) && !(isset($_SESSION['password']))){
-                        $_SESSION['username'] = $nameValue;
+                }if($bottom ===""){
+                    if(!(isset($_SESSION['loginid'])) && !(isset($_SESSION['password']))){
+                        $_SESSION['loginid'] = $nameValue;
                         $_SESSION['password'] = $passwordValue;
                     }
                     $form = <<< EOBODY
@@ -46,6 +46,7 @@ EOBODY;
                                 <td>Patient Name</td>
                                 <td>Diagnosis</td>
                                 <td>Priority Score</td>
+                                <td>Physician</td>
                                 <td>Status</td>
                                 <td>Processed By</td>
                             </tr>
@@ -59,7 +60,7 @@ EOBODY;
                 } 
             }
             $body = $form.$bottom;
-			echo $body;
+			echo $body
         ?>
     </body>
 	</div>		
--- newPatient.php
+++ newPatient.php
@@ -1,83 +1,45 @@
 <html>
     <head>
         <meta charset="utf-8"/>
+		<link rel="stylesheet" href="patientstyle.css">
 		<script src= "conditions.js"></script>
 		<script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <div>
+	<body>
+        <h1>New Patient Information</h1>
         <?php
-			session_start();
-		?>
-    </head>
-	<div class="tab">
-  <button class="tablinks" onclick="openFolder(event, 'Patient Information')">Patient Information</button>
-  <button class="tablinks" onclick="openFolder(event, 'Risk Factor Evaluation')">Risk Factor Evaluation</button>
-	</div>
-    <body>
-		<div id="Patient Information" class="tabcontent">
-        <h1>New Patient Information</h1
-        <?php
-			$conditions = json_decode($_POST["conditions"]);
-			echo $conditions;
             $form = "";
             if(!(isset($_POST['submit']))){
                 $form = <<< EOBODY
                     <form action="{$_SERVER['PHP_SELF']}" method="post">
                        <strong>First Name: </strong><input type="text" name="firstName" /><br /><br />
                        <strong>Last Name: </strong><input type="text" name="lastName"/><br /><br />
-                       <strong>SSN: </strong><input type = "text" name = "ssn"/><br /><br />
+                       <strong>DOB: </strong><input type = "number" value = "month"/>/<input type="number" value="day">/<input type="number" value="year"><br /><br />
                        
-                       <p>Patient Condition</p>
-					   
-					   Syncope<input type = "checkbox" name = "syncope" value = "Syncope"/><br /><br />
-					   
-                       
-                       
-                       <input type="submit" name="submit" value = "Submit" /><br/>
-                       
+                       <strong><p>Patient Condition</p></strong>
+					   Pulse Rate(bpm): <input type="number" name="pulse_rate"><br /><br />
+					   Systolic/Diastolic (mmHg): <input type="number" value="sbp">/<input type="number" value="dbp"><br /><br />
+					   SpO2 %: <input type="number" value="spo2 "><br /><br />
+					   Temperature: <input type="number" value="temp"><br /><br />
+					   Notes: <input type="text" name="lastName"/><br /><br />
+		</body>                      
                    </form>
-				   </div>
 EOBODY;
             }else{
 				$form = <<< EOBODY
 					<p> Patient information has been recorded </p>
 EOBODY;
 			}
-			
-			echo $form;
-			
-
-        ?>
-        
-    <form action = "main.php" method = "post">
-        <input type="submit" name="home" value = "Return to Home" /><br/>
-    </form>
-		</div>
-		<div id="Risk Factor Evaluation" class="tabcontent">
-        <h1>Risk Factor Evaluation</h1>
-        <?php
-            $form = "";
-            if(!(isset($_POST['submit']))){
-                $form = <<< EOBODY
-                    <form action="{$_SERVER['PHP_SELF']}" method="post">
-                       <strong>Age: </strong><input type="text" name="Age" /><br /><br />
-                       <strong>Diabetic </strong><input type="text" name="diabetic"/><br /><br />
-                       <strong>History of Heart Disease</strong><input type = "text" name = "hdHistory"/><br /><br />
- 	            <input type="submit" name="submit" value = "Submit" /><br/>
-                       
-                   </form>
-EOBODY;
-            }else{
-				$form = <<< EOBODY
-					<p> Risk Factor Evaluation has been recorded </p>
-EOBODY;
+			if(isset($_POST['pulse_rate'])){
+				getBP($_POST['pulse_rate']);
 			}
-			
 			echo $form;
+			
 
         ?>
         
-    <form action = "main.php" method = "post">
-        <input type="submit" name="home" value = "Return to Home" /><br/>
-    </form>
+    <form action = "Risk Factor.php" method = "post">
+        <input type="submit" name="home" value = "Proceed to Risk Factor" /><br/><br />
 		</div>
-    </body>
+		
 </html>

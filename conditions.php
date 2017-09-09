75b64728048310a55a4f67c7e25b2adcd52c6486
Sahana Rao <sahanarao.va@gmail.com>
2017-09-09 13:55:02 -0400

    conditions

---
+++ conditions.php
@@ -0,0 +1,8 @@
+<?php
+     $conditions = array("syncope"=> 5,"severe headache" => 3,"hypoxia" => 7, "low bp" => 3,"abdominal pain" => 4,
+    "vomiting" => 2, "diarrhea" => 2, "fatigue" => 4, "3rd degree burn" => 3,"chest pain" => 7,"high bp" => 3,
+    "unconscious" => 4, "difficulty breathing" => 9, "pulse rate < 40" => 10, "pulse rate > 120" => 10,
+    "spinal injury" => 10, "skull injury" => 10, "neck sprain" => 6, "back sprain" => 6, "rib fracture" => 6,
+    "severe open wound" => 4, "severe cough" => 1, "flu symptoms" => 1, "age" => 2, "diabetic" => 1,
+    "heart disease" => 1);
+?>

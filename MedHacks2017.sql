0c77fbce0fcb2ee11348652ee7c3032ad98ca2d9
Mohit Jauhri <Mohit@Mohits-MacBook-Pro.local>
2017-09-09 19:00:41 -0400

    MedHacks2017.sql

--- MedHacks2017.sql
+++ MedHacks2017.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE patientDB;
 
 CREATE TABLE PatientInfo(
-  PersonID int
-  LastName varchar(255)
-  FirstName varchar(255)
-  Address varchar(255)
-  City varchar(255)
+  PersonID int,
+  LastName varchar(255),
+  FirstName varchar(255),
+  Address varchar(255),
+  City varchar(255),
   DateofBirth date
 );
 
 CREATE TABLE PatientVitals(
-  SELECT CURRENT_TIMESTAMP
-  PulseRate int
-  SystolicBP int
-  DiastolicBP int
-  Oxygen Saturation(%) int
-  Temperature int
+  TimeStamp date
+  PulseRate int,
+  SystolicBP int,
+  DiastolicBP int,
+  OxygenSaturation, int
+  Temperature int,
   Notes varchar(255)
 );
 
 CREATE TABLE PatientRisks(
-  Age int
-  Diabetic varchar(255)
-  History of Heart Disease varchar(255)
+  Age int,
+  Diabetic varchar(255),
+  HistoryOfHeartDisease varchar(255)
 );
